f960042b4bcb7db75fb33002c6018fb7567c75d3
RTerBorch <robin.ter.borch@gmail.com>
2024-09-11 14:35:11 +0200

    Using livsmedelverkets API built connections and repos to save FoodItems to database.

--- Backend/src/main/java/com/robintb/food_fit/models/FoodItem.java
+++ Backend/src/main/java/com/robintb/food_fit/models/FoodItem.java
@@ -1,28 +1,29 @@
 package com.robintb.food_fit.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class FoodItem {
 
     @Id
     private Long id;
     private String name;
     private LocalDateTime version;
-    private double weightInGrams;
 
-    @OneToMany
-    private List<Nutrients> nutrientsList;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Nutrient> nutrientList;
 
-
-
-
+    public FoodItem(Long id, String name, LocalDateTime version, List<Nutrient> nutrientList) {
+        this.id = id;
+        this.name = name;
+        this.version = version;
+        this.nutrientList = nutrientList;
+    }
 }
--- Backend/src/main/java/com/robintb/food_fit/services/ImportFoodService.java
+++ Backend/src/main/java/com/robintb/food_fit/services/ImportFoodService.java
@@ -2,41 +2,113 @@ package com.robintb.food_fit.services;
 
 
 import com.robintb.food_fit.models.FoodItem;
+import com.robintb.food_fit.models.Nutrient;
+import com.robintb.food_fit.repositories.ImportFoodRepository;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @Service
 public class ImportFoodService {
 
+    @Autowired
+    private ImportFoodRepository importFoodRepository;
+
+    private final String baseUrl = "https://dataportal.livsmedelsverket.se/livsmedel";
     private static final HashSet<String> NUTRIENTS_OF_INTEREST = new HashSet<>();
 
     static {
-        NUTRIENTS_OF_INTEREST.add("Energi (kcal)");
-        NUTRIENTS_OF_INTEREST.add("Fett");
+        NUTRIENTS_OF_INTEREST.add("Energy (kcal)");
+        NUTRIENTS_OF_INTEREST.add("Fat, total");
         NUTRIENTS_OF_INTEREST.add("Protein");
-        NUTRIENTS_OF_INTEREST.add("Kolhydrater");
+        NUTRIENTS_OF_INTEREST.add("Carbohydrates, available");
     }
 
 
     public void collectFoodData(int foodLimit) {
 
+        String method = "GET";
+        String href = baseUrl + "/api/v1/livsmedel?offset=0&limit=" + foodLimit + "&sprak=2";
         List<FoodItem> foodItems = new ArrayList<>();
+        JSONObject foodItemJSONObject = (JSONObject) CollectJSONDataFromLink(href, method);
+
+        // DEBUG
+        System.out.println(foodItemJSONObject.keySet());
+        JSONArray livsmedelArray = (JSONArray) foodItemJSONObject.get("livsmedel");
+
+        // foodItem name
+
+        for (Object livsmedel : livsmedelArray) {
+
+            JSONObject foodItemJSONOBJECT = (JSONObject) livsmedel;
+            JSONArray linksArray = (JSONArray) foodItemJSONOBJECT.get("links");
+            String nutritionAPILink = "";
+            String nutritionAPIMethod = "";
+
+            // Prepare for FoodItem creation
+            Long foodItemId = Long.parseLong(foodItemJSONOBJECT.get("nummer").toString());
+            String foodItemName = foodItemJSONOBJECT.get("namn").toString();
+            LocalDateTime version = LocalDateTime.parse(foodItemJSONOBJECT.get("version").toString());
+            List<Nutrient> foodItemNutrients = new ArrayList<>();
+
+            // Collect URL from href tag and method for restAPI to collect nutrition data of footItem.
+            for (Object JSONLinks : linksArray) {
+                JSONObject linkObject = (JSONObject) JSONLinks;
+
+                System.out.println("rel:  " + linkObject.get("rel"));
+
+                if (linkObject.get("rel").equals("naringvarden")) {
+                    nutritionAPILink = baseUrl + linkObject.get("href").toString();
+                    nutritionAPIMethod = linkObject.get("method").toString();
+                }
+            }
+            JSONArray nutrientJSONArray = (JSONArray) CollectJSONDataFromLink(nutritionAPILink, nutritionAPIMethod);
+            System.out.println("Nutrient JSON object: " + nutrientJSONArray);
+
+            for (Object nutrientItem : nutrientJSONArray){
+                JSONObject nutrientJSONObject = (JSONObject) nutrientItem;
+
+                if (NUTRIENTS_OF_INTEREST.contains(nutrientJSONObject.get("namn"))){
+
+
+                    foodItemNutrients.add(new Nutrient(
+                            nutrientJSONObject.get("namn").toString(),
+                            nutrientJSONObject.get("enhet").toString(),
+                            Double.valueOf(nutrientJSONObject.get("varde").toString()),
+                            Double.valueOf(nutrientJSONObject.get("viktGram").toString())
+                            ));
+
+                    }
+                }
+            System.out.println(foodItemNutrients.size() + " Size");
+         //   FoodItem foodItem = new FoodItem(foodItemId, foodItemName,version,foodItemNutrients);
+          //  foodItems.add(foodItem);
+
+            updateFoodItemIfNewer(foodItemId, foodItemName,version,foodItemNutrients);
+           // importFoodRepository.save(foodItem);
+           // importFoodRepository.
+        }
+    }
+
+    private Object CollectJSONDataFromLink(String href, String method) {
+
+        System.out.println("Method: " + method);
+        System.out.println("Link: " + href);
 
         try {
-            URL url = new URL("https://dataportal.livsmedelsverket.se/livsmedel/api/v1/livsmedel?offset=0&limit="
-                    + foodLimit + "&sprak=2");
-
+            URL url = new URL(href);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
+            connection.setRequestMethod(method);
             connection.connect();
 
             int responseCode = connection.getResponseCode();
@@ -53,46 +125,21 @@ public class ImportFoodService {
                 }
                 scanner.close();
 
-                // Parse string into a JSON object
                 JSONParser jsonParser = new JSONParser();
-                JSONObject dataObject = (JSONObject) jsonParser.parse(inline);
+                Object parsedData = jsonParser.parse(inline); // Parse the JSON data into a generic Object
 
-                System.out.println(dataObject.keySet());
-                JSONArray livsmedelArray = (JSONArray) dataObject.get("livsmedel");
-
-
-                // foodItem name
-                for (Object livsmedel : livsmedelArray) {
-                    JSONObject foodItem = (JSONObject) livsmedel;
-                    System.out.println("version: " + foodItem.get("version"));
-                    System.out.println("livsmedelnamn: " + foodItem.get("namn"));
-                    JSONArray linksArray = (JSONArray) foodItem.get("links");
-
-                    String link = "";
-                    for (Object JSONLinks : linksArray){
-                        System.out.println("teeeest: " + JSONLinks);
-                        JSONObject linkObject = (JSONObject) JSONLinks;
-
-                        System.out.println("rel:  " + linkObject.get("rel"));
-
-                        if(linkObject.get("rel").equals("naringvarden")){
-                            link = linkObject.get("href").toString();
-                        }
-
-                    }
-                    System.out.println("Processed link:" + link);
-
-                    JSONObject nutrientLink = (JSONObject) linksArray.get(0);
-                    System.out.println(nutrientLink.get("href"));
-
-                    collectNutrientData(nutrientLink.get("href").toString());
+                if (parsedData instanceof JSONObject) {
+                    JSONObject jsonObject = (JSONObject) parsedData;
+                    System.out.println("It's a JSONObject!");
+                    return jsonObject;  // Return the JSONObject or handle it accordingly
+                } else if (parsedData instanceof JSONArray) {
+                    JSONArray jsonArray = (JSONArray) parsedData;
+                    System.out.println("It's a JSONArray!");
+                    return jsonArray;  // Return the JSONArray or handle it accordingly
+                } else {
+                    throw new IllegalArgumentException("Unexpected JSON type");
                 }
-
-
             }
-
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (ParseException e) {
@@ -102,24 +149,33 @@ public class ImportFoodService {
 
     }
 
-    private void collectNutrientData(String href) {
-        System.out.println("link: " + href);
-/*
+    public void updateFoodItemIfNewer(Long foodItemId, String foodItemName, LocalDateTime newVersion, List<Nutrient> foodItemNutrients) {
 
-        try {
-        URL url = new URL(href);
+        // Check if the food item already exists in the database
+        Optional<FoodItem> optionalFoodItem = importFoodRepository.findById(foodItemId);
 
-        HttpURLConnection connection = null;
+        if (optionalFoodItem.isPresent()) {
+            FoodItem existingFoodItem = optionalFoodItem.get();
 
-        connection.setRequestMethod("GET");
-        connection.connect();
+            // Compare the version
+            if (existingFoodItem.getVersion().isBefore(newVersion)) {
+                // The new version is more recent, update the fields
+                existingFoodItem.setName(foodItemName);
+                existingFoodItem.setVersion(newVersion);
+                existingFoodItem.setNutrientList(foodItemNutrients);
 
-            connection = (HttpURLConnection) url.openConnection();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+                // Save the updated food item
+                importFoodRepository.save(existingFoodItem);
+            } else {
+                // The existing version is the same or newer, no update required
+                System.out.println("The existing food item version is up to date.");
+            }
+        } else {
+            // If the food item doesn't exist, create a new one
+            FoodItem newFoodItem = new FoodItem(foodItemId, foodItemName, newVersion, foodItemNutrients);
+            importFoodRepository.save(newFoodItem);
         }
-*/
-
-
     }
+
 }
+
